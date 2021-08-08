f16402bb31f1165f56af041ee30008e3022e5277
Victor Machado <machado.prx@usp.br>
2021-08-08 14:19:59 -0300

    fixes, added bert embeddings and metrics

--- run.py
+++ run.py
@@ -1,17 +1,15 @@
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, roc_auc_score
 from sklearn.svm import SVC
 import numpy as np
 import pandas as pd
-from sentence_transformers import SentenceTransformer
 from graph_construction import graph_construction
 from regularizer import regularization
 from sklearn.metrics import classification_report
 
-model = SentenceTransformer('bert-base-uncased')
 it = 15
 mi = 1.0
 nb_neighbors = 30
-completions = [0.2,0.4,0.6,0.8,1.0]
+completions = [1.0,0.8,0.6,0.4,0.2]
 splits = 5
 seed = 0
 
@@ -20,7 +18,11 @@ np.random.seed(seed)
 '''
 intent:
 !git clone https://github.com/karansikka1/documentIntent_emnlp19/ ./dataset/
+!wget https://www.dropbox.com/s/pp1nkipzklrgqwl/paper-intent.zip
+!tar -xvf ./dataset/resnet18_feat.tar
+!unzip paper-intent.zip -d ./features
 '''
+
 labels = {"provoke":0, "inform":1, 
         "advocate":2, "entertain":3, 
         "expose":4, "express":5, 
@@ -44,14 +46,16 @@ for split in range(0, splits):
             X_test_im.append(features)
         X_test_im = np.array(X_test_im)
 
-        y_train = np.array(df_train['intent'].apply(lambda a: labels[a]))
-        y_test = np.array(df_test['intent'].apply(lambda a: labels[a]))
+        y_train = np.load(f'./features/train_target_{split}.npy')
+        y_train = np.argmax(y_train, axis=1)
+        y_test = np.load(f'./features/test_target_{split}.npy')
+        y_test = np.argmax(y_test, axis=1)
 
         incomplete_indices = np.random.randint(0, len(y_test), int(len(y_test) * (1.0-completion)))
         incomplete_indices += len(y_train)
 
-        X_train_txt = model.encode(df_train['caption'])
-        X_test_txt = model.encode(df_test['caption'])
+        X_train_txt = np.load(f'./features/train_fts_{split}.npy')
+        X_test_txt = np.load(f'./features/test_fts_{split}.npy')
         
         X_txt = np.concatenate([X_train_txt, X_test_txt], axis=0)
 
@@ -67,6 +71,18 @@ for split in range(0, splits):
 
         clf = SVC(gamma='auto', probability=True)
         clf.fit(X_train, y_train)
-        y_pred = clf.predict_proba(X_test)
-        y_pred = np.argmax(y_pred, axis=1)
-        print(classification_report(y_test, y_pred))
+        y_probs = clf.predict_proba(X_test)
+        y_pred = np.argmax(y_probs, axis=1)
+
+        result = {
+            'split':split,
+            'completion': completion,
+            'acc': accuracy_score(y_test, y_pred),
+            'auc': roc_auc_score(y_test, y_probs, multi_class='ovr')
+        }
+        print(result)
+        results.append(result)
+
+df_results = pd.DataFrame(results)
+df_results.to_csv('results_prop.csv')
+        
